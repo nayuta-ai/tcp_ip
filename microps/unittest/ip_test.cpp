49f9acd080587b97b621e7500d41c08c5a16786c
nayuta-ai <nayuta723@gmail.com>
2023-04-05 21:13:44 +0900

    Add step 6 test

--- microps/unittest/ip_test.cpp
+++ microps/unittest/ip_test.cpp
@@ -17,3 +17,25 @@ TEST(IpInitTest, FailuCase) {
 
   EXPECT_EQ(-1, result);
 }
+
+TEST(IpAddrPtonTest, ValidCase) {
+  ip_addr_t addr;
+
+  ASSERT_EQ(ip_addr_pton("192.168.1.1", &addr), 0);
+  EXPECT_EQ(192, ((uint8_t *)&addr)[0]);
+  EXPECT_EQ(168, ((uint8_t *)&addr)[1]);
+  EXPECT_EQ(1, ((uint8_t *)&addr)[2]);
+  EXPECT_EQ(1, ((uint8_t *)&addr)[3]);
+}
+
+TEST(IpAddrPtonTest, InvalidCase) {
+  ip_addr_t addr;
+  ASSERT_EQ(ip_addr_pton("256.168.1.1", &addr), -1);  // Out of range
+  ASSERT_EQ(ip_addr_pton("256.168.1", &addr), -1);
+  ASSERT_EQ(ip_addr_pton("256.168.1.1.1", &addr), -1);
+}
+
+TEST(IpAddrNtopTest, ValidCase) {
+  char p[200];
+  EXPECT_STREQ("255.255.255.255", ip_addr_ntop(0xffffffff, p, 32 * 4));
+}
